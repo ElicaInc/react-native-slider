94763861c7f3f421d18c20964f7d1d3b4d7872b7
elicaInc <plez.smile@me.com>
2025-01-25 23:23:35 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -876,6 +876,7 @@ export class Slider extends PureComponent<SliderProps, SliderState> {
                                             translateY: 0,
                                         },
                                     ],
+                                    zIndex: i === this._activeThumbIndex ? 1 : 0, //kwata
                                     ...valueVisibleStyle,
                                 },
                             ]}
